bc9633bc933566a59e486773786a2d84d3461e7a
Linsen <nesnil@gmail.com>
2023-06-19 21:23:30 +0800

    submit
    submit

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -28,7 +28,7 @@ const InfoCard: React.FC<{
         color: token.colorTextSecondary,
         lineHeight: '22px',
         padding: '16px 19px',
-        minWidth: '220px',
+        minWidth: '250px',
         flex: 1,
       }}
     >
@@ -77,14 +77,14 @@ const InfoCard: React.FC<{
         {desc}
       </div>
       <a href={href} target="_blank" rel="noreferrer">
-        了解更多 {'>'}
+        进入系统 {'>'}
       </a>
     </div>
   );
 };
 
 const Welcome: React.FC = () => {
-  const { token } = theme.useToken();
+  // const { token } = theme.useToken();
   const { initialState } = useModel('@@initialState');
   return (
     <PageContainer>
@@ -104,10 +104,10 @@ const Welcome: React.FC = () => {
             backgroundPosition: '100% -30%',
             backgroundRepeat: 'no-repeat',
             backgroundSize: '274px auto',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
+            //backgroundImage: "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
           }}
         >
+          {/*
           <div
             style={{
               fontSize: '20px',
@@ -128,31 +128,49 @@ const Welcome: React.FC = () => {
           >
             Ant Design Pro 是一个整合了 umi，Ant Design 和 ProComponents
             的脚手架方案。致力于在设计规范和基础组件的基础上，继续向上构建，提炼出典型模板/业务组件/配套设计资源，进一步提升企业级中后台产品设计研发过程中的『用户』和『设计者』的体验。
-          </p>
+          </p>*/}
           <div
             style={{
               display: 'flex',
               flexWrap: 'wrap',
-              gap: 16,
+              gap: 30,
             }}
           >
             <InfoCard
               index={1}
               href="https://umijs.org/docs/introduce/introduce"
-              title="了解 umi"
-              desc="umi 是一个可扩展的企业级前端应用框架,umi 以路由为基础的，同时支持配置式路由和约定式路由，保证路由的功能完备，并以此进行功能扩展。"
+              title="应用系统1"
+              desc="应用系统1描述"
             />
             <InfoCard
               index={2}
-              title="了解 ant design"
-              href="https://ant.design"
-              desc="antd 是基于 Ant Design 设计体系的 React UI 组件库，主要用于研发企业级中后台产品。"
+              href="https://umijs.org/docs/introduce/introduce"
+              title="应用系统1"
+              desc="应用系统1描述"
             />
             <InfoCard
               index={3}
-              title="了解 Pro Components"
-              href="https://procomponents.ant.design"
-              desc="ProComponents 是一个基于 Ant Design 做了更高抽象的模板组件，以 一个组件就是一个页面为开发理念，为中后台开发带来更好的体验。"
+              href="https://umijs.org/docs/introduce/introduce"
+              title="应用系统1"
+              desc="应用系统1描述"
+            />
+            <InfoCard
+              index={4}
+              href="https://umijs.org/docs/introduce/introduce"
+              title="应用系统1"
+              desc="应用系统1描述"
+            />
+            <InfoCard
+              index={5}
+              href="https://umijs.org/docs/introduce/introduce"
+              title="应用系统1"
+              desc="应用系统1描述"
+            />
+            <InfoCard
+              index={6}
+              href="https://umijs.org/docs/introduce/introduce"
+              title="应用系统1"
+              desc="应用系统1描述"
             />
           </div>
         </div>
